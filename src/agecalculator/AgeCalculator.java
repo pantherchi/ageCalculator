889dfb33bab55a52ffbf811f603fab85c12ef579
pantherChi <PANTHERCHI@GOAT.COM>
2017-03-18 16:55:57 +0000

    [03] : Found out that the method 01 of getting current date works better so cleaned up the code

--- src/agecalculator/AgeCalculator.java
+++ src/agecalculator/AgeCalculator.java
@@ -20,24 +20,17 @@ public class AgeCalculator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int[] myArray = dateArray();
+        int[] myArray = getDate();
         System.out.println(myArray[2]);
         
         //method 01
         Calendar cal = Calendar.getInstance();
         System.out.println(cal.getTime());
-        
-        SimpleDateFormat sdf = new SimpleDateFormat("Y:M:d");
-        System.out.println( sdf.format(cal.getTime()) );
-        
-        //method 02
-        Date date = new Date();
-        int cYear = date.getYear() + 1900;
-        int cMonth = date.getMonth() + 1;
-        int cDay = date.getDay();
-        System.out.println(cYear+"/"+cMonth+"/"+cDay);
+        System.out.println(cal.get(Calendar.YEAR));
+        System.out.println(cal.get(Calendar.MONTH));
+        System.out.println(cal.get(Calendar.DAY_OF_MONTH));
     }
-    public static int[] dateArray(){
+    public static int[] getDate(){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your date of birth (dd/mm/yyyy): ");
         String input = sc.nextLine();
@@ -51,16 +44,4 @@ public class AgeCalculator {
         sc.close();
         return intArray;
     }
-    public static int calc(){
-        Calendar cal = Calendar.getInstance();
-        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-        System.out.println( sdf.format(cal.getTime()) );
-    }
-
-//    public static void printArray(int[] array){
-//        for(int i = 0; i < array.length; i++){
-//            System.out.println(array[i]);
-//        }
-//    }
-    
 }
