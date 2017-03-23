bffcdbc72f2ffc2cc82f20170cd466516d4e6024
pantherChi <PANTHERCHI@GOAT.COM>
2017-03-23 16:05:09 +0000

    [06] Worked on Leap Year.
    Dates such as 29th of Feb.

--- src/agecalculator/AgeCalculator.java
+++ src/agecalculator/AgeCalculator.java
@@ -1,47 +1,93 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package agecalculator;
 
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Scanner;
 
-/**
- *
- * @author Vid-Tech
- */
 public class AgeCalculator {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+    public static void main (String[] args){
         int[] myArray = getDate();
-        System.out.println(myArray[2]);
-        
-        //method 01
+        int bDay = myArray[0];
+        int bMonth = myArray[1];
+//        int bYear = myArray[2];
+//        System.out.println("Date of birth: "+bYear + "/" + bMonth + "/" + bDay);
+       
         Calendar cal = Calendar.getInstance();
-        System.out.println(cal.getTime());
-        System.out.println(cal.get(Calendar.YEAR));
-        System.out.println(cal.get(Calendar.MONTH));
-        System.out.println(cal.get(Calendar.DAY_OF_MONTH));
-    }
-    public static int[] getDate(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your date of birth (dd/mm/yyyy): ");
-        String input = sc.nextLine();
-        String[] stringArray = input.split("/");
-        final int SIZE = stringArray.length;
-        int[] intArray = new int[SIZE];
+        int cYear = cal.get(Calendar.YEAR);
+        int cMonth = cal.get(Calendar.MONTH) + 1;
+        int cDay = cal.get(Calendar.DAY_OF_MONTH);
+//        System.out.println("Current date: "+cYear + "/" + cMonth + "/" + cDay);
         
-        for (int i = 0; i < SIZE; i++) {
-            intArray[i] = Integer.parseInt(stringArray[i]);
+        //to calculate days till
+        Date bDate = new Date(cYear,bMonth,bDay);
+        Date cDate = new Date(cYear,cMonth,cDay);
+        daysTill(bDate, cDate, bDay, bMonth, cYear);
+    }
+    //this method will get the input from the user!
+    public static int[] getDate(){
+        int[] intArray;
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter your date of birth (dd/mm/yyyy): ");
+            String input = sc.nextLine();
+            String[] stringArray = input.split("/");
+            final int SIZE = stringArray.length;
+            intArray = new int[SIZE];
+            for (int i = 0; i < SIZE; i++) {
+                intArray[i] = Integer.parseInt(stringArray[i]);
+            }
         }
-        sc.close();
         return intArray;
+    }
+    //this method will take two date objects to undergo arithmetics
+    public static void daysTill(Date bD, Date cD,int bDay, int bMonth, int cYear){        
+    	int comparison = bD.compareTo(cD);
+        if(bMonth == 2 && bDay == 29){
+            if(isLeapYear(cYear) == true){
+                System.out.println("Current is a leap year");
+            }else{
+                System.out.println("Current is not a leap year");
+                for(int i = 1;i <= 3 ; i++){
+                    boolean b = isLeapYear(cYear + i);
+                    if(b == true){
+//                        System.out.println("Year " + (cYear + i) +" is a leap year.");
+                        System.out.println("Next leap year is in "+ i + " year.");
+                        
+                        int years = cYear + i;
+                        leapYearCalc(i,years, cD);
+//                        Date dDate = new Date(years,bMonth,bDay);
+//                        System.out.println("Your birthday is in " + (years.getTime()-cD.getTime())/(1000*60*60*24) + " days.");
+                        
+                    }else{
+                        i++;
+                    }
+                }
+            }
+        }else{
+            switch (comparison) {
+                case 1:
+                    //do the calculation
+                    System.out.println("Your birthday is in " + (bD.getTime()-cD.getTime())/(1000*60*60*24) + " days.");
+                    break;
+                case 0:
+                    //both same date
+                    System.out.println("Happy Birthday!");
+                    break;
+                default:
+                    //the date is in the future???
+                    System.out.println("Your birthday is in " + (365-(bD.getTime()-cD.getTime())/(1000*60*60*24)) + " days.");
+                    break;
+            }
+        }
+    }
+    //checks if the next year is a leap year
+    public static boolean isLeapYear(int Year){
+        return (Year % 4 == 0 && Year % 100 != 0) || (Year % 400 == 0);        
+    }
+    public static void leapYearCalc(int i, int y,Date cD){
+        Date dDate = new Date(2028,2,29);
+        System.out.println((dDate.getTime())/(1000*60*60*24));
+        System.out.println((cD.getTime())/(1000*60*60*24));
+        int x = (i * 365);
+        System.out.println("Your birthday is in " + ((((dDate.getTime()-cD.getTime())/(1000*60*60*24))+2) + " days."));
     }
 }
